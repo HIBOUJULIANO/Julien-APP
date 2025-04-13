a9d2033ceff7de4c79a5c82b8f656c91905cf2b6
HIBOUJULIANO <43265697+HIBOUJULIANO@users.noreply.github.com>
2025-04-13 16:29:19 +0000

    Mise à jour de l'application Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,36 @@
 import streamlit as st
+import pydeck as pdk
 
-st.title("Hello Streamlit-er 👋")
+
+st.title("Hello World 👋")
 st.markdown(
     """ 
-    This is a playground for you to try Streamlit and have fun. 
-
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
+    git add streamlit_app.py
+git commit -m "Mise à jour de l'application Streamlit"
+git push origin main
     """
 )
 
 if st.button("Send balloons!"):
     st.balloons()
-    
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=48.85,
+        longitude=2.333333,
+        zoom=50,
+        pitch=20,
+    ),
+layers=[
+  pdk.Layer(
+            'ScatterplotLayer',
+            
+            data=[
+                {'position': [2.333333, 48.85], 'size': 1},
+               
+            ],
+            get_color='[200, 30, 0, 160]',
+            get_radius=5        ),
+],
+    ))
