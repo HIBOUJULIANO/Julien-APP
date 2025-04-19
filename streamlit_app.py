25e8ec8c214612ac2ac69ff451973d2a5736317a
HIBOUJULIANO <43265697+HIBOUJULIANO@users.noreply.github.com>
2025-04-19 15:15:02 +0000

    Mise à jour de l'application Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.markdown(
     """ 
    Hello, welcome to the Casino of the emojis.
    In this virtual casino you can win virtual emojis if you have 
-   4 similar emojis
+   4 similar emojis. This game was created with Random and Time Librairies.
     """
 )
 
