c251f493c8fccc99a4b8b37ab34dcd87109f05ad
HIBOUJULIANO <43265697+HIBOUJULIANO@users.noreply.github.com>
2025-05-09 15:07:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ url = "https://www.julien-hoarau.com/Aribnb.csv"
 # Lire le fichier CSV depuis l'URL
 df = pd.read_csv(url)
 
-st.title (Recherche dans une base de donnée)
+st.title ("Recherche dans une base de donnée")
 st.write ("select your Neighbourhood")
 options = ['Popincourt', 'Reuilly', 'Louvre']
 choixquartier = st.selectbox("👤 Neighbourhood:", options)
