a62c2e92afb879cee53a6bd76b16aa477cdc7877
HIBOUJULIANO <43265697+HIBOUJULIANO@users.noreply.github.com>
2025-05-09 14:23:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ df = pd.read_csv(url)
 
 # Afficher les premières lignes du fichier
 #st.dataframe(df)
-recherche = df[df['id'] == '1']['name']
-resultat = df['name'].values[0]
+recherche = df[df['id'] == '3']['name'].values[0]
+resultat = df['name']
 st.write(recherche)
 st.write(resultat)
 
